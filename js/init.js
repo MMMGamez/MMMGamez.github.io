dc3d129f5f6f0e53b0b97b265048c4f0f50fbbef
Mark Meyer <mark.meyer37@gmail.com>
2014-07-12 15:34:48 -0700

    Got first alternate page working

--- js/init.js
+++ js/init.js
@@ -7,12 +7,12 @@
 skel.init({
 	reset: 'full',
 	breakpoints: {
-		global:		{ range: '*', href: 'css/style.css', containers: 1400, grid: { gutters: 50 } },
-		wide:		{ range: '-1680', href: 'css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
-		normal:		{ range: '-1280', href: 'css/style-normal.css', containers: 960, lockViewport: true },
-		narrow:		{ range: '-980', href: 'css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
-		narrower:	{ range: '-840', href: 'css/style-narrower.css', grid: { collapse: 1 } },
-		mobile:		{ range: '-640', href: 'css/style-mobile.css', containers: '100%', grid: { gutters: 15, collapse: 2 } }
+		global:		{ range: '*', href: '/css/style.css', containers: 1400, grid: { gutters: 50 } },
+		wide:		{ range: '-1680', href: '/css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
+		normal:		{ range: '-1280', href: '/css/style-normal.css', containers: 960, lockViewport: true },
+		narrow:		{ range: '-980', href: '/css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
+		narrower:	{ range: '-840', href: '/css/style-narrower.css', grid: { collapse: 1 } },
+		mobile:		{ range: '-640', href: '/css/style-mobile.css', containers: '100%', grid: { gutters: 15, collapse: 2 } }
 	}
 }, {
 	layers: {
@@ -112,6 +112,10 @@ skel.init({
 			});
 
 		// Header.
+		if (window.location.origin + "/" === window.location.href) {
+			$header.addClass('alt');
+		}
+
 		// If the header is using "alt" styling and #banner is present, use scrollwatch
 		// to revert it back to normal styling once the user scrolls past the banner.
 		// Note: This is disabled on touch devices and whenever the 'normal' breakpoint is
